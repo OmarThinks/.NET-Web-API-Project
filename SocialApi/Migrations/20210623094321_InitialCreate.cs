cc0edb03791c9563f0c702972a80cf0eed741449
Omar Magdy <contact.parmag@gmail.com>
2021-06-23 11:43:59 +0200

    migrated and scaffolded the Post Model

---
+++ SocialApi/Migrations/20210623094321_InitialCreate.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SocialApi.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Post",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Content = table.Column<string>(type: "TEXT", nullable: true),
+                    ReleaseDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Post", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Post");
+        }
+    }
+}
--- SocialApi/Startup.cs
+++ SocialApi/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace SocialApi
 {
@@ -32,6 +33,9 @@ namespace SocialApi
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "SocialApi", Version = "v1" });
             });
+
+            services.AddDbContext<SocialApiContext>(options =>
+                    options.UseSqlite(Configuration.GetConnectionString("SocialApiContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
